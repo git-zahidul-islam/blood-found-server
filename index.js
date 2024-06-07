c9c136666021e3a70fb4cb15c49887bc81ae9349
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-06-07 17:53:23 +0600

    donation delete api done

--- index.js
+++ index.js
@@ -127,6 +127,13 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/donationDelete/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await donationCollection.deleteOne(query)
+            res.send(result)
+        })
+
         // specific user data get api
         app.get('/donation/:email', async (req, res) => {
             const email = req.params.email;
